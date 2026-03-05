9ecce00266cd90c835c51c22881908a68db2fdfe
Brendan <bcain150@gmail.com>
2026-03-04 23:23:31 -0500

    add some aliases and helpful env vars

---
+++ bash_scripts/aliases.sh
@@ -0,0 +1,5 @@
+alias source_boxes="source ${BOXES_WS_ROOT}/install/local_setup.bash"
+alias cbs="colcon build --symlink-install"
+alias cbsps="colcon build --symlink-install --packages-select"
+alias source_jazzy="source /opt/ros/jazzy/setup.bash"
+alias clean_boxes_build='rm -rf build/ install/ log/'
