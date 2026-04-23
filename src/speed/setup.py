1fcec3da3faa5714434ece0f3df4fd4ff657074e
Brendan <bcain150@gmail.com>
2026-04-23 00:18:12 -0400

    wip: starting motor control node

--- src/speed/setup.py
+++ src/speed/setup.py
@@ -10,7 +10,7 @@ setup(
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=["setuptools", "boxes_interfaces", "boxes_utils", "pyvesc"],
+    install_requires=["setuptools", "boxes_interfaces", "boxes_utils"],
     zip_safe=True,
     maintainer="bcain",
     maintainer_email="bcain150@gmail.com",
--- src/speed/speed/motor_control_node.py
+++ src/speed/speed/motor_control_node.py
@@ -4,21 +4,44 @@ from rclpy.executors import SingleThreadedExecutor
 from rclpy import logging as ros_logger
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 
-
 from boxes_interfaces.msg import XboxVelocityInput
-from boxes_utils import VOLATILE_QOS
+from boxes_utils.qos import VOLATILE_QOS
+
+from speed.vesc_uart import VescCommandInterface
+
+UART_PORT = "/dev/ttyAMA0"
+BAUD_RATE = 115200
+LEFT_CAN_ID = 96
 
 
 class MotorControlInterface(Node):
     def __init__(self):
         super().__init__("motor_control")
+        
+        self.motor_movement_timer = self.create_timer(
+            callback=self.control_motors
+        )
 
         self.teleop_subscriber = self.create_subscription(
             msg_type=XboxVelocityInput,
             topic="controller_move_state",
+            callback=self.read_control_states,
             qos_profile=VOLATILE_QOS,
             callback_group=MutuallyExclusiveCallbackGroup(),
         )
+
+        self.vesc = VescCommandInterface(
+            uart_port=UART_PORT,
+            baud_rate=BAUD_RATE,
+            secondary_can_id=LEFT_CAN_ID,
+        )
+        self.vesc.connect()
+
+    def read_control_states(self, data):
+        pass
+
+    def control_motors(self):
+        pass
 
 
 def main(args=None):
