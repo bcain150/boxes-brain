90c09ef21dc4ed78b1f71b7d068ba0dda0febdc4
Brendan <bcain150@gmail.com>
2026-04-24 00:36:06 -0400

    get teleop node built and running bare minimum

--- README.md
+++ README.md
@@ -8,11 +8,31 @@ wireless xbox controller dongle
 - make sure to also install the required dependencies which they don't call out (cabextract and DKMS since we're on a lite version of the linux distro
 - `sudo apt install joystick` - this installs useful tools for testing out the interface such as `evtest` and `fftest`
 
+NOTE: I experienced in the next step that it seemed to unregister xone from dkms for some reason... 
+- I had to reregister xone with dkms and then reinstall specific linux headers, after doing so I verified that UART works still. Secondly install the meta-package headers and it should auto update and not break again
+ - `sudo apt install linux-headers-6.8.0-1052-raspi`
+ - meta headers: `sudo apt install linux-headers-raspi` _this auto rebuilt xone and reinstalled without dkms_
+
+enabling uart on GPIO pins 14 & 15 (this is for raspberry pi5)
+1. edit config.txt in `/boot/firmware/config.txt`
+    - add the following lines:
+        - `enable_uart=1`
+        - `dtoverlay=uart0-pi5`
+        - `dtoverlay=disable-bt` _not sure how crucial this is_
+2. disable the debug console for ubuntu from constantly writing to /dev/tty/AMA0
+    - remove the following text from `/boot/firmware/cmdline.txt` (make sure it stays on one line, no extra spaces or `,`):
+        - `console=serial0,115200`
+3. stop the serial getty service which allows people to login from uart
+    - `sudo systemctl stop serial-getty@ttyAMA0.service`
+    - `sudo systemctl mask serial-getty@ttyAMA0.service`
+4. reboot
+    - `sudo reboot`
+
+
 ros distro
 - use ros2 jazzy
 - had to reflash pi's os with ubuntu 24.04 because I didn't want to have to deal with docker containers
 - followed (https://docs.ros.org/en/jazzy/How-To-Guides/Installing-on-Raspberry-Pi.html#ubuntu-linux-on-raspberry-pi-with-binary-ros-2-install)[these instructions]
 - also (https://docs.ros.org/en/jazzy/How-To-Guides/Installing-on-Raspberry-Pi.html#ubuntu-linux-on-raspberry-pi-with-binary-ros-2-install)[these]
 
-todo:
-- how to enable uart on gpio pins on a pi5 running. 24.04 headless noble
+
--- src/teleop/teleop/xbox_interface_node.py
+++ src/teleop/teleop/xbox_interface_node.py
@@ -7,7 +7,7 @@ from evdev import InputDevice, list_devices, ecodes
 import rclpy
 from rclpy.node import Node
 from rclpy.executors import MultiThreadedExecutor
-from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup, ReentrantCallbackGroup
 from rclpy import logging as ros_logger
 
 from boxes_interfaces.msg import XboxOutput, XboxInput
@@ -15,24 +15,32 @@ from boxes_utils import VOLATILE_QOS, format_error_message
 
 from teleop.controller import ControllerState, Button, VENDOR_ID, PRODUCT_IDS
 
-INPUT_PUBLISHING_RATE_S = 0.02
+INPUT_PUBLISHING_RATE_S = 0.01
 
 
 class XboxControllerInterface(Node):
     def __init__(self):
         super().__init__("xbox_control_interface")
 
+        state_read_group = MutuallyExclusiveCallbackGroup()
+        input_output_group = MutuallyExclusiveCallbackGroup()
+
         # controller read timer is a loop inside a loop so just immediately restart it
-        self.controller_read_timer = self.create_timer(0, self.read_controller)
+        self.controller_read_timer = self.create_timer(
+            timer_period_sec=0,
+            callback=self.read_controller,
+            callback_group=state_read_group
+        )
         self.teleop_publishing_timer = self.create_timer(
-            INPUT_PUBLISHING_RATE_S, self.send_teleop
+            timer_period_sec=INPUT_PUBLISHING_RATE_S,
+            callback=self.send_teleop,
+            callback_group=input_output_group
         )
 
         self.teleop_publisher = self.create_publisher(
             msg_type=XboxInput,
             topic="controller_move_state",
-            qos_profile=VOLATILE_QOS,
-            callback_group=MutuallyExclusiveCallbackGroup(),
+            qos_profile=VOLATILE_QOS
         )
 
         # allows other nodes to provide feedback
@@ -41,13 +49,17 @@ class XboxControllerInterface(Node):
             topic="controller_feedback",
             callback=self.write_controller,
             qos_profile=VOLATILE_QOS,
-            callback_group=MutuallyExclusiveCallbackGroup(),
+            callback_group=input_output_group,
         )
 
         # a place to store the instance of the connected controller
         self.controller: Optional[InputDevice] = None
         self.controller_state: Optional[ControllerState] = None
-        self.connected = False
+        self.first_read_success = False
+
+    @property
+    def connected(self):
+        return self.controller is not None
 
     def _connect_to_controller(self):
         while self.controller is None:
@@ -63,25 +75,28 @@ class XboxControllerInterface(Node):
                         device=device, node_logger=self.get_logger()
                     )
                     break
+            if self.connected:
+                break
             self.get_logger().warning("No device found!")
-            time.sleep(3)
+            time.sleep(1)
         self.get_logger().info("Controller Connected!")
 
     def read_controller(self):
         """ROS timer loop which constantly reads events coming from the controller. If the controller disconnects,
         it moves to a disconnected state and will constantly attempt to reconnect"""
 
-        if not self.controller:
+        if not self.connected:
             # spin inside this loop until a controller is disconnected
             self._connect_to_controller()
-            self.connected = True
 
         try:
             for event in self.controller.read_loop():
                 if event.type == ecodes.EV_SYN:
+                    self.get_logger().debug("SYN RECEIVED")
                     continue
                 self.get_logger().debug(f"Received event -> {event}")
                 self.controller_state.update(event)
+                self.first_read_success = True
         except OSError:
             # clear controller instance since it has been disconnected
             self.controller = None
@@ -90,20 +105,26 @@ class XboxControllerInterface(Node):
             self.get_logger().error(
                 f"An unhandled exception occured in the controller read loop! ->\n{format_error_message(e)}"
             )
-        finally:
-            # mark as disconnected for state publishing
-            self.connected = False
 
     def write_controller(self, data):
+        #TODO: implement rumbling
         pass
 
     def send_teleop(self):
-        # TODO make this account for stale packets
-        input_state = XboxInput(
-            connected=self.connected,
-            left_stick_y=self.controller_state.get_button(Button.LEFT_STICK_Y),
-            right_stick_y=self.controller_state.get_botton(Button.RIGHT_STICK_Y)
-        )
+        if not self.first_read_success:
+            return
+        try:
+            input_state = XboxInput(
+                connected=self.connected,
+                left_stick_y=self.controller_state.get_normalized(Button.LEFT_STICK_Y),
+                right_stick_y=self.controller_state.get_normalized(Button.RIGHT_STICK_Y)
+            )
+            self.get_logger().debug("Publishing Controller Input State...")
+        except Exception as e:
+            self.get_logger().error(f"Error Publishing Controller State! ->\n {format_error_message(e)}")
+
+        self.teleop_publisher.publish(input_state)
+
 
 
 def main(args=None):
@@ -122,6 +143,10 @@ def main(args=None):
         ros_logger.get_logger("executor_logger").warning(
             "Keyboard Interrupt - Shutting down Xbox Control Interface!"
         )
+    finally:
+        xbox_node.destroy_node()
+        executor.shutdown()   # stops spinning, joins internal threads
+        rclpy.try_shutdown()
 
 
 if __name__ == "__main__":
