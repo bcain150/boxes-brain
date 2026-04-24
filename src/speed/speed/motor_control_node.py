a1da2ca37e261fcf0d9c53b81dae04317f4b52a8
Brendan <bcain150@gmail.com>
2026-04-24 01:10:58 -0400

    WIP: motor control node pre build

--- src/speed/speed/motor_control_node.py
+++ src/speed/speed/motor_control_node.py
@@ -1,11 +1,13 @@
+from threading import RLock
+
 import rclpy
 from rclpy import Node
-from rclpy.executors import SingleThreadedExecutor
+from rclpy.executors import MultiThreadedExecutor
 from rclpy import logging as ros_logger
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 
-from boxes_interfaces.msg import XboxVelocityInput
-from boxes_utils.qos import VOLATILE_QOS
+from boxes_interfaces.msg import XboxInput
+from boxes_utils import VOLATILE_QOS, format_error_message
 
 from speed.vesc_uart import VescCommandInterface
 
@@ -13,21 +15,27 @@ UART_PORT = "/dev/ttyAMA0"
 BAUD_RATE = 115200
 LEFT_CAN_ID = 96
 
+MOTOR_STATE_READ_S = 0.5
 
 class MotorControlInterface(Node):
     def __init__(self):
         super().__init__("motor_control")
-        
-        self.motor_movement_timer = self.create_timer(
-            callback=self.control_motors
+
+        motor_control_group = MutuallyExclusiveCallbackGroup()
+        motor_read_group = MutuallyExclusiveCallbackGroup()
+
+        self.motor_state_timer = self.create_timer(
+            timer_period_sec=MOTOR_STATE_READ_S,
+            callback=self.read_motors,
+            callback_group=motor_read_group
         )
 
         self.teleop_subscriber = self.create_subscription(
-            msg_type=XboxVelocityInput,
+            msg_type=XboxInput,
             topic="controller_move_state",
-            callback=self.read_control_states,
+            callback=self.control_motors,
             qos_profile=VOLATILE_QOS,
-            callback_group=MutuallyExclusiveCallbackGroup(),
+            callback_group=motor_control_group,
         )
 
         self.vesc = VescCommandInterface(
@@ -37,19 +45,56 @@ class MotorControlInterface(Node):
         )
         self.vesc.connect()
 
-    def read_control_states(self, data):
-        pass
+        self._serial_lock = RLock()
 
-    def control_motors(self):
-        pass
+    def read_motors(self):
+        motor_fields = ["temp_motor", "temp_fet", "rpm", "fault_code", "duty_now"]
+        with self._serial_lock:
+            try:
+                self.get_logger().info("Getting left and right motor status...")
+                right_motor_status = self.vesc.get_status(*motor_fields)
+                left_motor_status = self.vesc.get_status(*motor_fields, to_can=True)
+            except Exception as e:
+                self.get_logger().warning(f"Error occured while getting motor status -> \n{format_error_message(e)}")
+            self.get_logger().debug(f"-- RIGHT MOTOR STATUS --\n{right_motor_status}")
+            self.get_logger().debug(f"-- LEFT MOTOR STATUS --\n{left_motor_status}")
 
+        # TODO: do something with these statuses (probably smothing and normalization)
+
+    def control_motors(self, data):
+        self.get_logger().info("Xbox Button Input Received...")
+        left_motor_duty = data.left_stick_y
+        right_motor_duty = data.right_stick_y
+        is_connected = data.connected
+        errored = False
+        with self._serial_lock:
+            if is_connected:
+                try:
+                    self.vesc.set_duty(right_motor_duty)
+                except Exception as e:
+                    self.get_logger().error(f"Error setting right motor duty! ->\n{format_error_message(e)}")
+                    errored = True
+                try:
+                    self.vesc.set_duty(left_motor_duty, to_can=True)
+                    errored = True
+                except Exception as e:
+                    self.get_logger().error(f"Error setting left motor duty! ->\n{format_error_message(e)}")
+            else:
+                try:
+                    self.get_logger().warning("Controller disconnected - stopping motors...")
+                    self.vesc.stop_all()
+                except Exception as e:
+                    self.get_logger().error(f"Error stopping motors! ->\n{format_error_message(e)}")
+        
+        if not errored:
+            self.get_logger().info("Successfully set motor control...")
 
 def main(args=None):
     print("Starting Motor Control Node...")
     rclpy.init(args=args)
     motor_control = MotorControlInterface()
 
-    executor = SingleThreadedExecutor()
+    executor = MultiThreadedExecutor(num_threads=2)
     executor.add_node(motor_control)
     motor_control.get_logger().info("Motor Control Node Intialized.")
 
@@ -59,6 +104,10 @@ def main(args=None):
         ros_logger.get_logger("executor_logger").warning(
             "Keyboard Interrupt - Shutting down Motor Control"
         )
+    finally:
+        motor_control.destroy_node()
+        executor.shutdown()   # stops spinning, joins internal threads
+        rclpy.try_shutdown()
 
 
 if __name__ == "__main__":
